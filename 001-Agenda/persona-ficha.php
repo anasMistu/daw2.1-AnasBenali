6c4a4e05ca496dc42eb6916de42d8a2412fb4ccc
Anas Benali <anas.benali@educa.madrid.org>
2020-11-19 17:19:36 +0100

    Nav hecho

--- 001-Agenda/persona-ficha.php
+++ 001-Agenda/persona-ficha.php
@@ -50,7 +50,6 @@ $rsCategoria = $selectCategoria->fetchAll();
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 
