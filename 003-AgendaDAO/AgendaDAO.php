a5d1ee1fe34e5b6581a8d6b400264c252792c0c5
Anas Benali <anas.benali@educa.madrid.org>
2020-12-16 18:46:42 +0100

    AgendaDAO.

---
+++ 003-AgendaDAO/AgendaDAO.php
@@ -0,0 +1,62 @@
+<?php
+require_once "classes.php";
+
+class AgendaDAO
+{
+    private static $pdo = null;
+
+    private static function obtenerPdoConexionBD()
+    {
+        $servidor = "localhost";
+        $identificador = "root";
+        $contrasenna = "";
+        $bd = "agenda"; // Schema
+        $opciones = [
+            PDO::ATTR_EMULATE_PREPARES => false, // Modo emulación desactivado para prepared statements "reales"
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Que los errores salgan como excepciones.
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // El modo de fetch que queremos por defecto.
+        ];
+
+        try {
+            $pdo = new PDO("mysql:host=$servidor;dbname=$bd;charset=utf8", $identificador, $contrasenna, $opciones);
+        } catch (Exception $e) {
+            error_log("Error al conectar: " . $e->getMessage());
+            exit("Error al conectar" . $e->getMessage());
+        }
+
+        return $pdo;
+    }
+
+    private static function ejecutarConsulta(string $sql, array $parametros): array
+    {
+        if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
+
+        $select = self::$pdo->prepare($sql);
+        $select->execute($parametros);
+        return $select->fetchAll();
+    }
+
+    private static function ejecutarActualizacion(string $sql, array $parametros): void
+    {
+        if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
+
+        $actualizacion = self::$pdo->prepare($sql);
+        $actualizacion->execute($parametros);
+    }
+
+    private static function crearCategoriaDesdeRs(array $rs): Categoria
+    {
+        return new Categoria($rs[0]["id"], $rs[0]["nombre"]);
+    }
+
+    public static function categoriaObtenerTodos(): ?Categoria
+    {
+        if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
+        $sql="SELECT * FROM categoria";
+        $select = self::$pdo->prepare($sql);
+        $select->execute();
+        $rs=$select->fetchAll();
+        if ($rs) return self::crearCategoriaDesdeRs($rs);
+        else return null;
+    }
+}
